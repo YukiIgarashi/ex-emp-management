83984cc0dd1b7c1f1f872dd1e85d24893c514beb
YukiIgarashi <yuki.igarashi@rakus-partners.co.jp>
2021-11-04 13:37:53 +0900

    サービスクラスに管理者新規登録のメソッド追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 @Service
@@ -13,4 +14,14 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 	
+	
+	/**
+	 * 管理者新規登録のメソッド
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		
+		administratorRepository.insert(administrator);
+		
+	}
 }
