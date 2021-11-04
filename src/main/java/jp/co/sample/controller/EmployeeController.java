9db4a1989c53c87d0674270dd9cdc29c8c0d15ed
YukiIgarashi <yuki.igarashi@rakus-partners.co.jp>
2021-11-04 15:25:43 +0900

    従業員リストのHTML作成

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package jp.co.sample.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員用コントローラークラス
+ * @author igayuki
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	/**
+	 * 従業員一覧ページへ遷移させるメソッド
+	 * @param model　従業員リストを入れるスコープ
+	 * @return　従業員一覧ページ
+	 */
+	public String showList(Model model) {
+		
+		List<Employee> employeeList = employeeService.showList();
+		model.addAttribute("employeeList",employeeList);
+		return "employee/list";
+		
+	}
+
+}
